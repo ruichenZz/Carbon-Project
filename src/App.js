84e3dc69dbcdd503886b87edeee4f5fe91f751b1
yjh020711@ucla.edu <yjh020711@ucla.edu>
2022-06-20 22:29:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
       {/* <Nav /> */}
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        <Route path="/edit/:id" component={Editor} />
-        <Route path="/edit_grapesjs/:id" component={GrapesJsEditor} />
+        {/* <Route path="//:id" component={Editor} /> */}
+        <Route path="/edit/:id" component={GrapesJsEditor} />
         <Route path="/photo" component={PhotoInitials} />
         <Route path="/staff" component={StaffList} />
         <Route path="/linebreak" component={Linebreak} />

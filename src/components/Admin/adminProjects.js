a59fd94db2a8224af20fa0570065507176734fcd
yjh020711@ucla.edu <yjh020711@ucla.edu>
2022-07-23 14:59:41 -0700

    updated file syntax

--- src/components/Admin/adminProjects.js
+++ src/components/Admin/adminProjects.js
@@ -1,21 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
-import { mediaQueries } from '../../shared/config';
-import config from '../../config';
-import { useHistory } from 'react-router-dom';
-import Modal from 'react-modal';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { mediaQueries } from "../../shared/config";
+import config from "../../config";
+import { useHistory } from "react-router-dom";
+import Modal from "react-modal";
 
 //material UI components
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
+/**
+ * Component Declaration
+ */
+function Project(props) {
+  const history = useHistory();
+  const { data } = props;
+
+  const handleApproveProjects = (id) => {
+    axios
+      .post(config.SERVER_URL + `/api/admin/approve`, {
+        id: id,
+      })
+      .then((res) => {
+        alert(res.data);
+        window.location.reload();
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  const handleDenyProjects = (id) => {
+    axios
+      .post(config.SERVER_URL + `/api/admin/deny`, {
+        id: id,
+      })
+      .then((res) => {
+        alert(res.data);
+        window.location.reload();
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  return (
+    <>
+      <TableRow>
+        <TableCell>{data.name}</TableCell>
+        <TableCell>{data.status}</TableCell>
+        <TableCell>
+          <Button
+            size="small"
+            variant="outlined"
+            onClick={() => history.push(`/edit/${data["_id"]}`)}
+          >
+            View
+          </Button>
+        </TableCell>
+        <TableCell>
+          {data.isApproved ? (
+            <Button size="small" color="success" variant="outlined" disabled>
+              Approve
+            </Button>
+          ) : (
+            <Button
+              size="small"
+              color="success"
+              variant="outlined"
+              onClick={() => handleApproveProjects(data["_id"])}
+            >
+              Approve
+            </Button>
+          )}
+        </TableCell>
+        <TableCell>
+          {data.status == "denied" ? (
+            <Button size="small" color="error" variant="outlined" disabled>
+              Deny
+            </Button>
+          ) : (
+            <Button
+              size="small"
+              color="error"
+              variant="outlined"
+              onClick={() => handleDenyProjects(data["_id"])}
+            >
+              Deny
+            </Button>
+          )}
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
 
 // Component declaration
 
@@ -25,23 +106,21 @@ const AdminProjects = (props) => {
   useEffect(() => {
     function getProject() {
       axios
-        .get(config.SERVER_URL + '/api/admin/projects/')
-        .then((res) => setProjects((res.data).allProjects))
+        .get(config.SERVER_URL + "/api/admin/projects/")
+        .then((res) => setProjects(res.data.allProjects.reverse()))
         .catch((err) => console.log(err));
     }
     getProject();
   }, []);
 
   const handleUpdateProject = () => {
-    console.log(projects)
-    setProjects(projects.map());
+    console.log(projects);
+    setProjects([...projects]);
   };
-
 
   return (
     <div>
-
-      {(
+      {
         <Container>
           <TableContainer component={Paper}>
             <Table>
@@ -68,7 +147,7 @@ const AdminProjects = (props) => {
             </Table>
           </TableContainer>
         </Container>
-      )}
+      }
     </div>
   );
 };
@@ -90,7 +169,7 @@ const Buttons = styled.button`
 
 const Container = styled.div`
   ${
-    '' /* background: #444444;
+    "" /* background: #444444;
   display: grid;
   grid-template-columns: 50% 50%;
   padding: 2em 0;
@@ -101,149 +180,4 @@ const Container = styled.div`
   }
   }
 `;
-
-const NoProjectContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: calc(100vh - 20vh);
-`;
 export default AdminProjects;
-
-/**
- * Component Declaration
- */
-function Project(props) {
-    //const [ApproveProjectModalOpen, setApproveProjectModalOpen] = useState(false);
-    const history = useHistory();
-  
-    const modalCustomStyles = {
-      content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-      },
-    };
-  
-    const { data } = props;
-  
-    const handleApproveProjects = (id) => {
-      axios
-        .post(config.SERVER_URL + `/api/admin/approve`, {
-            id: id,
-          })
-        .then((res) => {
-          alert(res.data);
-          window.location.reload();
-          //props.handleUpdateProject();
-          //setApproveProjectModalOpen(false);
-        })
-        .catch((err) => alert(err.message));
-    };
-
-    const handleDenyProjects = (id) => {
-        axios
-          .post(config.SERVER_URL + `/api/admin/deny`, {
-              id: id,
-            })
-          .then((res) => {
-            alert(res.data);
-            window.location.reload();
-            //props.handleUpdateProject();
-            //setApproveProjectModalOpen(false);
-          })
-          .catch((err) => alert(err.message));
-      };
-  
-    return (
-      <>
-        {/* <TableContainer component={Paper}>
-        <Table  aria-label="simple table"> */}
-  
-        <TableRow>
-          <TableCell>{data.name}</TableCell>
-          <TableCell>{data.status}</TableCell>
-          <TableCell>
-            
-            <Button size="small" variant="outlined" onClick={() => history.push(`/edit/${data['_id']}`)}>
-              View
-            </Button>
-  
-  
-          </TableCell>
-          <TableCell>
-            {data.isApproved ? 
-                <Button size="small" color="success" variant="outlined" disabled>
-                    Approve
-                </Button> : 
-                <Button size="small" color="success" variant="outlined" onClick={() => handleApproveProjects(data['_id'])}>
-                    Approve
-                </Button> 
-              }
-          </TableCell>
-          <TableCell>
-            {data.status == "denied" ? 
-                <Button size="small" color="error" variant="outlined" disabled>
-                    Deny
-                </Button> : 
-                <Button size="small" color="error" variant="outlined" onClick={() => handleDenyProjects(data['_id'])}>
-                    Deny
-                </Button> 
-              }
-          </TableCell>
-        </TableRow>
-        {/* </Table>
-      </TableContainer> */}
-        {/* <Modal
-          isOpen={ApproveProjectModalOpen}
-          onRequestClose={() => setApproveProjectModalOpen(false)}
-          ariaHideApp={false}
-          style={modalCustomStyles}
-        >
-          <p>Are you sure you want to delete this project?</p>
-          <button onClick={() => setApproveProjectModalOpen(false)}>Cancel</button>
-          <button onClick={() => handleDeleteProject(data['_id'])}>Delete</button>
-        </Modal> */}
-      </>
-    );
-  }
-  
-  /**
-   * Styled components declaration
-   */
-  const ProjectContainer = styled.div`
-    width: 80%;
-    margin: auto;
-    ${mediaQueries.tablet} {
-      width: 90%;
-    }
-    ${mediaQueries.mobile} {
-      width: 80%;
-    }
-  `;
-  
-  const TestPreview = styled.div`
-    height: 400px;
-    background: #bbbbbb;
-    position: relative;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 5px 5px 0 0;
-    color: gray;
-  `;
-  
-  const Description = styled.div`
-    border-radius: 0 0 5px 5px;
-    color: white;
-    padding: 0.5em 1em 1.5em 1em;
-    background: #333333;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  `;
